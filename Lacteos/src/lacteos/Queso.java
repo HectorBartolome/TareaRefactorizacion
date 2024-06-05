b44cfd1316eb3be8d6cca582fa8588e623b5f81d
Hector Bartolome <h.bartolomeo@fp.safavalladolid.com>
2024-06-05 13:03:40 +0200

    Ejercicio: 1.3 Version: 1.03

--- Lacteos/src/lacteos/Queso.java
+++ Lacteos/src/lacteos/Queso.java
@@ -20,5 +20,54 @@ public class Queso {
 		this.peso = peso;
 	}
 
+	/**
+	 * 
+	 */
+	public Queso() {
+		super();
+	}
+
+	/**
+	 * @return the tipo_leche
+	 */
+	public String getTipo_leche() {
+		return tipo_leche;
+	}
+
+	/**
+	 * @param tipo_leche the tipo_leche to set
+	 */
+	public void setTipo_leche(String tipo_leche) {
+		this.tipo_leche = tipo_leche;
+	}
+
+	/**
+	 * @return the cantidad_leche
+	 */
+	public int getCantidad_leche() {
+		return cantidad_leche;
+	}
+
+	/**
+	 * @param cantidad_leche the cantidad_leche to set
+	 */
+	public void setCantidad_leche(int cantidad_leche) {
+		this.cantidad_leche = cantidad_leche;
+	}
+
+	/**
+	 * @return the peso
+	 */
+	public double getPeso() {
+		return peso;
+	}
+
+	/**
+	 * @param peso the peso to set
+	 */
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
 	
 }
