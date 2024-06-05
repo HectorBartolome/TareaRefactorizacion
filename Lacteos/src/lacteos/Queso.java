5d055d98ff49cef798bbc97b63292072093e1f94
Hector Bartolome <h.bartolomeo@fp.safavalladolid.com>
2024-06-05 13:04:47 +0200

    Ejercicio: 1.4 Version: 1.04

--- Lacteos/src/lacteos/Queso.java
+++ Lacteos/src/lacteos/Queso.java
@@ -68,6 +68,11 @@ public class Queso {
 	public void setPeso(double peso) {
 		this.peso = peso;
 	}
-	
-	
+	public void imprimir () {
+		System.out.println("QUESOS ENTREPINARES");
+		System.out.println("Registro Sanitario No 52/38751");
+		System.out.println("Para consultar el lote del producto revise la etiqueta");
+		System.out.println("Peso: " + this.peso); System.out.println("Tipo de leche: " +
+		this.tipo_leche);
+		System.out.println("Cantidad de leche: " + this.cantidad_leche); }
 }
